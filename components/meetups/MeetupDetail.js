e21efaa234973339cd356703f8bbad79c3917c8c
santosh peddinti <santoshpeddinti1997@gmail.com>
2022-08-06 20:33:31 +0530

    'updated'

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,10 @@
 import classes from "./MeetupDetail.module.css";
-
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt={props.title} width={300} height={200} />
+      <picture>
+        <img src={props.image} alt={props.title} />
+      </picture>
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
-
+import Image from "next/image";
 function MeetupItem(props) {
   const router = useRouter();
 
@@ -13,7 +13,9 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} width={300} height={350} />
+          <picture>
+            <img src={props.image} alt={props.title} />
+          </picture>
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
